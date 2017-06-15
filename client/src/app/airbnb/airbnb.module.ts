3d60336f0e2de7decee6fd0a219a9171cbd8d13a
Maxime Denoun <maxime_denoun@hotmail.com>
2017-06-15 10:23:33 +0300

    add component to be cleaner

--- client/src/app/airbnb/airbnb.module.ts
+++ client/src/app/airbnb/airbnb.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AirbnbComponent } from './airbnb.component';
 import {SharedModule} from "../shared/shared.module";
 import {AirbnbService} from "./airbnb.service";
+import { AirbnbCardComponent } from './airbnb-card/airbnb-card.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule
   ],
-  declarations: [AirbnbComponent],
+  declarations: [AirbnbComponent, AirbnbCardComponent],
   exports: [AirbnbComponent],
   providers: [AirbnbService]
 })
